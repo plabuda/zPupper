6ef16921677395cdf30c06bfda90ec14155ebbd7
plabuda <25438387+plabuda@users.noreply.github.com>
2021-12-29 18:50:10 +0100

    Format code and add package.json

--- main.js
+++ main.js
@@ -5,46 +5,44 @@ let illo = new Zdog.Illustration({
   element: '.inner',
 });
 
-let center = Dot('#900',{x: 0, y: 0, z: 0});
-let oldCenter = {x : 0, y : 0, z : 0};
+let center = Dot('#900', { x: 0, y: 0, z: 0 });
+let oldCenter = { x: 0, y: 0, z: 0 };
 let points = [];
 
-function reducer (prev, curr) 
-{
-return      {'x': prev.x + curr.translate.x,
-            'y': prev.y + curr.translate.y,
-            'z': prev.z + curr.translate.z};
+function reducer(prev, curr) {
+  return {
+    'x': prev.x + curr.translate.x,
+    'y': prev.y + curr.translate.y,
+    'z': prev.z + curr.translate.z
+  };
 }
 
-function Dot(color, translation)
-{
-   return new Zdog.Shape({
-  addTo: illo,
-  stroke: 25,
-  color: color,
-  translate: translation
-        });
+function Dot(color, translation) {
+  return new Zdog.Shape({
+    addTo: illo,
+    stroke: 25,
+    color: color,
+    translate: translation
+  });
 }
 
 
-function addPoint(x, y, z)
-{
-    points.push(Dot('#F93',{x, y, z}));
-    var res = points.reduce(reducer, {x: 0, y : 0, z : 0});
-    console.log(res);
-    center.translate.x = res.x / points.length;
-    center.translate.y = res.y / points.length;
-    center.translate.z = res.z / points.length;
+function addPoint(x, y, z) {
+  points.push(Dot('#F93', { x, y, z }));
+  var res = points.reduce(reducer, { x: 0, y: 0, z: 0 });
+  console.log(res);
+  center.translate.x = res.x / points.length;
+  center.translate.y = res.y / points.length;
+  center.translate.z = res.z / points.length;
 
 }
 
-function addRandomPoint()
-{
-    addPoint(
-        Math.random() * 300 - 150,
-        Math.random() * 300 - 150,
-        Math.random() * 300 - 150
-    )
+function addRandomPoint() {
+  addPoint(
+    Math.random() * 300 - 150,
+    Math.random() * 300 - 150,
+    Math.random() * 300 - 150
+  )
 }
 
 // create illo
@@ -63,16 +61,14 @@ function animate() {
   illo.updateRenderGraph();
 
   // animate next frame
-  requestAnimationFrame( animate );
+  requestAnimationFrame(animate);
 }
 
-function populate(amount)
-{
-    if(amount > 0)
-    {
-        addRandomPoint();
-        setTimeout(() => populate(amount -1), 900);
-    }
+function populate(amount) {
+  if (amount > 0) {
+    addRandomPoint();
+    setTimeout(() => populate(amount - 1), 900);
+  }
 }
 
 populate(10);
