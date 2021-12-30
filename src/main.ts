bcc1be7a1c42be2574c5969fc9c6fac1a103a90a
plabuda <25438387+plabuda@users.noreply.github.com>
2021-12-30 17:57:40 +0100

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,7 @@
 import * as Zdog from 'zdog';
-import * as VectorUtils from './vector_utils';
 import { Universe } from './verlet_universe';
-import * as zdog_module from './zdog-point';
 
 let uni = new Universe('.inner');
-
-function Dot(color: string, translation: { x: any; y: any; z: any; }) {
-  return new Zdog.Shape({
-    stroke: 25,
-    color: color,
-    translate: translation
-  });
-}
-
-function addPoint(x: number, y: number, z: number) {
-  let dot = Dot('hsla(100, 60%, 50%, 1)', { x, y, z });
-  uni.AddShape(dot);
-}
 
 function addRandomPoint() {
   uni.AddDot(
@@ -30,13 +15,10 @@ function addRandomPoint() {
   )
 }
 
-// create illo
-
 function animate() {
 
   uni.illo.rotate.y += 0.01;
   uni.Render(1 / 60);
-  // animate next frame
   requestAnimationFrame(animate);
 }
 
@@ -49,4 +31,3 @@ function populate(amount: number) {
 
 populate(30);
 animate();
-zdog_module.helloWorld();
